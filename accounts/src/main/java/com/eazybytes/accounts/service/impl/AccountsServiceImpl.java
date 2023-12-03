913e28f5b78c805541af16877b4ddb94a1f84099
nileshzarkar <nileshzarkar@gmail.com>
2023-12-03 15:20:27 +0530

    commit

--- accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
+++ accounts/src/main/java/com/eazybytes/accounts/service/impl/AccountsServiceImpl.java
@@ -37,8 +37,8 @@ public class AccountsServiceImpl implements IAccountsService {
        if(optionalCustomer.isPresent()) {
          throw new CustomerAlreadyException("Customer already registered with given mobile number: "+ customerDto.getMobileNumber());
        }
-       customer.setCreatedAt(LocalDateTime.now());
-       customer.setCreatedBy("Anonymous");
+       //customer.setCreatedAt(LocalDateTime.now());
+       //customer.setCreatedBy("Anonymous");
        Customer savedCustomer = customerRepository.save(customer);
        accountsRepository.save(createNewAccount(savedCustomer));
     }
@@ -51,8 +51,8 @@ public class AccountsServiceImpl implements IAccountsService {
         newAccount.setAccountNumber(randomAccntNUmber);
         newAccount.setAccountType(AccountsConstants.SAVINGS);
         newAccount.setBranchAddress(AccountsConstants.ADDRESS);
-        newAccount.setCreatedAt(LocalDateTime.now());
-        newAccount.setCreatedBy("Anonymous");
+        //newAccount.setCreatedAt(LocalDateTime.now());
+        //newAccount.setCreatedBy("Anonymous");
         return newAccount;
     }
 
